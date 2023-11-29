7e87e6d09ee6a80542961857057baebdc5a124e4
Elena Kryshtal <elenkrishtal@gmail.com>
2023-11-29 11:42:29 +0200

    Create custom button component using props and dynamic class

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Button from 'primevue/button';
+
+import { ref, defineProps } from 'vue';
+
+const props = defineProps(['customClass']);
+
+const customClass = ref(props.customClass);
+</script>
+
+<template>
+  <Button type="button" :class="customClass" icon="pi pi-check" enter>
+    <slot></slot>
+  </Button>
+</template>
+
+<style lang="scss" scoped></style>
